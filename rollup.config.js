0534fecbe63fb31abdc71d851196e8bdff7794a3
Dan Delany <daniel.t.delany@jpl.nasa.gov>
2025-02-20 02:46:13 -0800

    modify rollup config to resolve node modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+
 export default [
   {
     treeshake: true,
     input: 'out-tsc/index.js',
     output: {
       dir: 'dist'
-    }
+    },
+    plugins: [
+      resolve(),  // this resolves imports from node_modules
+    ],
   }
 ]
--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,36 @@ import { ActionParameters } from './models/action-parameters.js';
 import { ActionSettings } from './models/action-settings.js';
 // import { ActionMain } from 'aerie-action/src/models/main.ts';
 
+import {listFiles, readFile, writeFile} from 'aerie-actions/dist/helpers.js';
+
 export async function main(actionParameters: ActionParameters, actionSettings: ActionSettings) {
-  // Make a dummy request.
-  const result = await fetch(`${actionSettings.externalUrl}/${actionParameters.sequenceId}`, {
+  const url = `${actionSettings.externalUrl}/${actionParameters.sequenceId}`;
+  const startTime = performance.now();
+
+  // Make a request to an external URL using fetch
+  const result = await fetch(url, {
     method: 'get',
     headers: {
       'Content-Type': 'application/json'
     }
   });
+  console.log(`request took ${performance.now() - startTime}ms`);
 
-  const resultText = await result.text();
+  // try parsing result as either json or text
+  let resultData: string;
+  try {
+    resultData = await result.clone().json();
+  } catch {
+    resultData = await result.clone().text();
+  }
+
+  // read/write files using the actions helpers
+  const files = listFiles();
+  const myFile = readFile('my_file');
+  writeFile('new_file', 'new contents');
 
   return {
     status: "SUCCESS",
-    data: resultText
+    data: resultData
   };
 }
