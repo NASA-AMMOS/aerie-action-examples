4abebef09e2f090a8859a75d628f0efb7432a553
jhaug <jhaug@jpl.nasa.gov>
2025-07-22 15:50:02 -0700

    Updated test for file-action

--- examples/file-action/tests/main.test.ts
+++ examples/file-action/tests/main.test.ts
@@ -2,7 +2,8 @@ import { main } from "../src";
 
 import assert from 'node:assert';
 import { test, mock } from "node:test";
-import type {ActionsAPI, ReadSequenceResult} from "@nasa-jpl/aerie-actions";
+import type {ActionsAPI} from "@nasa-jpl/aerie-actions";
+import {ReadDictionaryResult, ReadParcelResult} from "@nasa-jpl/aerie-actions/dist/types/db-types";
 
 // The basic-action example makes a `fetch` call to an external URL when it runs.
 // This test avoids relying on making an actual call to an external URL by "mocking"
@@ -28,41 +29,78 @@ const mockFetch = mock.fn(async (url, options) => {
 
 // TS utility that lets us mock only selected parts of an object,
 // while treating it as a fully-typed instance of the whole thing.
-function createMock<T extends object>(overrides: Partial<{ [K in keyof T]: T[K] }>): T {
+function createMock<T extends object>(overrides: {
+  copyFile: (source: string, dest: string) => Promise<void>;
+  deleteFile: (source: string) => Promise<void>;
+  readDictionaryFile(filePath: string): Promise<string>;
+  getEnvironmentVariable(name: string): (string | undefined);
+  readParameterDictionary(id: number): Promise<ReadDictionaryResult>;
+  moveFile: (source: string, dest: string) => Promise<void>;
+  readCommandDictionary(id: number): Promise<ReadDictionaryResult>;
+  readFile: (name: string) => Promise<void>;
+  readParcel(id: number): Promise<ReadParcelResult>;
+  createDirectory: (name: string) => Promise<void>;
+  readChannelDictionary(id: number): Promise<ReadDictionaryResult>;
+  writeFile: (name: string, definition: string, overwrite: boolean) => Promise<void>;
+  listFiles: (path: string) => Promise<any[]>;
+  createDirectories: (name: string) => Promise<void>
+}): T {
   return overrides as T;
 }
 
 // create a partial mock of the actions API, so we can test it without making real database calls
 // TODO: extract createMockActionsAPI into aerie-actions TestUtils package
 const mockActionsAPI = createMock<ActionsAPI>({
-  listSequences: async () => {
-    return [];
+  readChannelDictionary(id: number): Promise<ReadDictionaryResult> {
+    return Promise.resolve(undefined);
+  }, getEnvironmentVariable(name: string): string | undefined {
+    return undefined;
+  }, readCommandDictionary(id: number): Promise<ReadDictionaryResult> {
+    return Promise.resolve(undefined);
+  }, readDictionaryFile(filePath: string): Promise<string> {
+    return Promise.resolve("");
+  }, readParameterDictionary(id: number): Promise<ReadDictionaryResult> {
+    return Promise.resolve(undefined);
+  }, readParcel(id: number): Promise<ReadParcelResult> {
+    return Promise.resolve(undefined);
   },
-  readSequence: async () => {
-    console.log("called readSequence");
-    return { id: 1, definition: "test" } as ReadSequenceResult;
-  },
-  writeSequence: async () => {},
   readFile: async (name: string) => {},
-  writeFile: async (name: string, definition: string) => {},
-  listFiles: async () => {
-    return [];
-  }
+  writeFile: async (name: string, definition: string, overwrite: boolean) => {},
+  listFiles: async (path: string) => {return []},
+  copyFile: async (source: string, dest: string) => {},
+  moveFile: async (source: string, dest: string) => {},
+  deleteFile: async (source: string) => {},
+  createDirectory: async (name: string) => {},
+  createDirectories: async (name: string) => {}
 });
 
-test("aerie basic example action", async (t) => {
+test("aerie files example action", async (t) => {
   t.mock.method(globalThis, 'fetch', mockFetch);
 
   await t.test("runs main", async () => {
     await main(
       {
-        urlPath: "repos/NASA-AMMOS/aerie",
-        myBool: false,
-        sleepMs: 0,
       },
       {
-        externalUrl: "https://api.github.com",
-        retries: 0,
+        copyFile: true,
+        copyFilePath: "dir1/test.json",
+        copyFileDest: "dir1/copy.json",
+        readFile: undefined,
+        readFilePath: undefined,
+        readDirectory: undefined,
+        readDirectoryPath: undefined,
+        writeFile: undefined,
+        writeFilePath: undefined,
+        writeFileContents: undefined,
+        writeFileOverwrite: undefined,
+        writeDirectory: undefined,
+        writeDirectoryPath: undefined,
+        writeDirectoryOverwrite: undefined,
+        moveFile: undefined,
+        moveFilePath: undefined,
+        moveFileDest: undefined,
+        deleteFile: undefined,
+        deleteFilePath: undefined,
       },
       mockActionsAPI,
     );
